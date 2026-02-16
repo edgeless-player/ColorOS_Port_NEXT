21f081dce2190b454541732cd22e35b3490cbe19
server <xiejiayu0902@163.com>
2026-02-16 23:59:20 +0800

    修复了c16 503的人脸识别，内嵌了4.19.325 ksunext内核

--- port.sh
+++ port.sh
@@ -42,29 +42,14 @@ else
     pack_type=EROFS
 fi
 
-# 检查为本地包还是链接
-if [ ! -f "${baserom}" ] && [ "$(echo $baserom |grep http)" != "" ];then
-    blue "底包为一个链接，正在尝试下载" "Download link detected, start downloding.."
-    aria2c --max-download-limit=1024M --file-allocation=none -s10 -x10 -j10 ${baserom}
-    baserom=$(basename ${baserom} | sed 's/\?t.*//')
-    if [ ! -f "${baserom}" ];then
-        error "下载错误" "Download error!"
-    fi
-elif [ -f "${baserom}" ];then
+if [ -f "${baserom}" ];then
     green "底包: ${baserom}" "BASEROM: ${baserom}"
 else
     error "底包参数错误" "BASEROM: Invalid parameter"
     exit
 fi
 
-if [ ! -f "${portrom}" ] && [ "$(echo ${portrom} |grep http)" != "" ];then
-    blue "移植包为一个链接，正在尝试下载"  "Download link detected, start downloding.."
-    aria2c --max-download-limit=1024M --file-allocation=none -s10 -x10 -j10 ${portrom}
-    portrom=$(basename ${portrom} | sed 's/\?t.*//')
-    if [ ! -f "${portrom}" ];then
-        error "下载错误" "Download error!"
-    fi
-elif [ -f "${portrom}" ];then
+if [ -f "${portrom}" ];then
     green "移植包: ${portrom}" "PORTROM: ${portrom}"
 else
     error "移植包参数错误" "PORTROM: Invalid parameter"
@@ -1012,10 +997,10 @@ fi
 yellow "删除多余的App" "Debloating..." 
 # List of apps to be removed
 
-debloat_apps=("HeartRateDetect" "Browser")
+debloat_apps=("HeartRateDetect" "Browser" "ConsumerIRApp" "ADS" "KeKePay" "OplusSecurityKeyboard" "Pictorial" "LinktoWindows" "KeKeAppDetail")
 #kept_apps=("Clock" "FileManager" "KeKeThemeSpace" "SogouInput" "Weather" "Calendar")
 #kept_apps=("BackupAndRestore" "Calculator2" "Calendar" "Clock" "FileManager" "OppoNote2" "OppoWeather2" "UPTsmService" "Music")
-kept_apps=("OppoNote2" "OppoWeather2")
+kept_apps=("Weather" "Calendar" "Clock" "FileManager" "OppoNote2" "OppoWeather2")
 #kept_apps=()
 
 if [[ $super_extended == "true" ]] && [[ $pack_method == "stock" ]] && [[ -f build/baserom/images/reserve.img ]]; then
